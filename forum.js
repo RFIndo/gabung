d20f9dc66847db22ee2071e335bbb9eaa82c1f4c
maknyusblogger <jegerdotkom@gmail.com>
2017-07-08 21:10:52 +0700

    Create forum.js

---
+++ forum.js
@@ -0,0 +1,12 @@
+var Digital=new Date()
+var hours=Digital.getHours()
+if (hours>=0&&hours<=5)
+document.write("<link rel='stylesheet' type='text/css' href='https://cdn.rawgit.com/RFIndo/forumb/654ebd84/stayle.css'>")
+else if (hours>=6&&hours<=12)
+document.write("<link rel='stylesheet' type='text/css' href='https://cdn.rawgit.com/RFIndo/foruma/8df966c4/stayle.css'>")
+else if (hours>=13&&hours<=17)
+document.write("<link rel='stylesheet' type='text/css' href='https://cdn.rawgit.com/RFIndo/foruma/8df966c4/stayle.css'>")
+else if (hours>=18&&hours<=23)
+document.write("<link rel='stylesheet' type='text/css' href='https://cdn.rawgit.com/RFIndo/forumb/654ebd84/stayle.css'>")
+
+<!--***Javascript by Joko Ariyanto FU***//--> 
